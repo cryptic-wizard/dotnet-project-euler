02bf00b775bb83a8c56295a3eff33dc29ed4f944
cryptic-wizard <87053379+cryptic-wizard@users.noreply.github.com>
2022-08-06 21:57:07 -0500

    Implemented problem 10

--- ProjectEuler/ProjectEuler.cs
+++ ProjectEuler/ProjectEuler.cs
@@ -14,6 +14,7 @@ namespace ProjectEuler
             { 7, Problem7 },
             { 8, Problem8 },
             { 9, Problem9 },
+            { 10, Problem10 },
         };
 
         /// <summary>
@@ -24,6 +25,7 @@ namespace ProjectEuler
             { 7, 104743 },
             { 8, 23514624000 },
             { 9, 31875000 },
+            { 10, 142913828922 },
         };
 
         /// <summary>
