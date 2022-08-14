9403a60ed0f9ac2bea1c7a6042a1ad5bf8c00e84
cryptic-wizard <87053379+cryptic-wizard@users.noreply.github.com>
2022-08-13 22:27:06 -0500

    Implemented problem 29 (#25)

---
+++ ProjectEuler/1-50/Problem029.cs
@@ -0,0 +1,38 @@
+﻿using ProjectEuler.Utilities;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+
+namespace ProjectEuler
+{
+    public static partial class ProjectEuler
+    {
+        /// <summary>
+        /// <see href="https://projecteuler.net/problem=29"/>
+        /// </summary>
+        public static long Problem029()
+        {
+            const int NUMBER = 100;
+            BigInteger value;
+            Dictionary<BigInteger, BigInteger> sequence = new();
+
+            for (int a = 2; a <= NUMBER; a++)
+            {
+                // 4 corners per spiral incremented by 2 * ring number
+                for (int b = 2; b <= NUMBER; b++)
+                {
+                    value = BigInteger.Pow(a, b);
+
+                    if (!sequence.ContainsKey(value))
+                    {
+                        sequence.Add(value, value);
+                    }
+                }
+            }
+
+            return sequence.Count;
+        }
+    }
+}
