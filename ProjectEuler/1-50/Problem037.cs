df3e3fa9fa4d5a1dd52629f340a6d88cdc19738b
cryptic-wizard <87053379+cryptic-wizard@users.noreply.github.com>
2022-08-17 22:33:53 -0500

    Implemented problem 37 (#30)

---
+++ ProjectEuler/1-50/Problem037.cs
@@ -0,0 +1,46 @@
+﻿using ProjectEuler.Utilities;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using static ProjectEuler.Utilities.Palindrome;
+
+namespace ProjectEuler
+{
+    public static partial class ProjectEuler
+    {
+        /// <summary>
+        /// <see href="https://projecteuler.net/problem=37"/>
+        /// </summary>
+        public static long Problem037()
+        {
+            const int NUMBER = 1000000;
+            const int PRIME_COUNT = 11;
+            const int MINIMUM_PRIME = 7;
+            long sum = 0, count = 0;
+            List<long> primes = Prime.GetPrimesUpTo(NUMBER);
+
+            foreach (long prime in primes)
+            {
+                if (prime <= MINIMUM_PRIME)
+                {
+                    continue;
+                }
+
+                if (Prime.IsTruncatable(prime))
+                {
+                    sum += prime;
+                    count++;
+
+                    if (count == PRIME_COUNT)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            return sum;
+        }
+    }
+}
